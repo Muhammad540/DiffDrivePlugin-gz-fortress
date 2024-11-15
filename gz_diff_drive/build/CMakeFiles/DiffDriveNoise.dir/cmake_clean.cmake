f518fb520d414d9dd9bc1e478c24dd5ca2808228
Muhammad540 <thisisdeahmed@gmail.com>
2024-11-15 14:25:32 +0300

    add plugin

---
+++ gz_diff_drive/build/CMakeFiles/DiffDriveNoise.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DiffDriveNoise.dir/DiffDriveNoise.cc.o"
+  "CMakeFiles/DiffDriveNoise.dir/DiffDriveNoise.cc.o.d"
+  "libDiffDriveNoise.pdb"
+  "libDiffDriveNoise.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DiffDriveNoise.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
